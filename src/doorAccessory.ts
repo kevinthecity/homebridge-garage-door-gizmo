126183841510853e87608ff524e0fe9dc614c3d2
Kevin Grant <kevingrant5@gmail.com>
2023-07-02 04:12:31 +0000

    Back to switch

--- src/doorAccessory.ts
+++ src/doorAccessory.ts
@@ -17,29 +17,40 @@ export class DoorAccessory {
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-    this.service = this.accessory.getService(this.platform.Service.Doorbell) ||
-                   this.accessory.addService(this.platform.Service.Doorbell);
+    this.service = this.accessory.getService(this.platform.Service.Switch) ||
+                   this.accessory.addService(this.platform.Service.Switch);
 
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
 
-    this.service.getCharacteristic(this.platform.Characteristic.ProgrammableSwitchEvent)
-      .onGet(this.handleButtonPress.bind(this));
+    this.service.getCharacteristic(this.platform.Characteristic.On)
+      .onGet(this.handleOnGet.bind(this))
+      .onSet(this.handleOnSet.bind(this));
   }
 
-  async handleButtonPress() {
-    const requestIpAndPath = `http://${this.platform.config.request_ip}/${this.platform.config.path}`;
-    this.platform.log.info(`Sending request to ${requestIpAndPath}...`);
-    try {
-      const response = await axios.get(requestIpAndPath);
-      if (response.status === 200) {
-        this.platform.log.info('Request was successful!');
-      }else{
-        this.platform.log.info('Wasn`t a 200 but also didn`t throw error');
-      }
-    } catch (e) {
-      this.platform.log.info(`Error while trying to send GET request to ${requestIpAndPath}`, e);
-    }
+  async handleOnGet() {
+    return false;  // Always off when requested, as it turns itself off automatically
+  }
 
-    return 0; // No event to return, as this acts like a stateless button
+  async handleOnSet(value) {
+    if (value) {
+      const requestIpAndPath = `http://${this.platform.config.request_ip}/${this.platform.config.path}`;
+      this.platform.log.info(`Sending request to ${requestIpAndPath}...`);
+      try {
+        const response = await axios.get(requestIpAndPath);
+        if (response.status === 200) {
+          this.platform.log.info('Request was successful!');
+        }else{
+          this.platform.log.info('Wasn`t a 200 but also didn`t throw error');
+        }
+      } catch (e) {
+        this.platform.log.info(`Error while trying to send GET request to ${requestIpAndPath}`, e);
+      }
+
+      // Automatically turn off the switch after a short delay
+      setTimeout(() => {
+        this.service.getCharacteristic(this.platform.Characteristic.On).updateValue(false);
+        this.platform.log.info('Switch off timeout');
+      }, 100);
+    }
   }
 }
