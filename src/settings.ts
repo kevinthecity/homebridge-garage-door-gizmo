10a89ef038d5ae7085fd48d649cd1fac2ed2b8ec
Kevin Grant <kevingrant5@gmail.com>
2023-10-08 20:39:51 -0400

    Rename the plugin

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeHttpButton';
+export const PLATFORM_NAME = 'GarageDoorGizmo';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'http-door-opener';
+export const PLUGIN_NAME = 'garage-door-gizmo';
